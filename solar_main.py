a3a4995e88a5eff360b7816f492f77fe68194de5
Tanya Tomashuk <tanyatomashuk@MacBook-Pro-Tanya.local>
2020-11-06 18:27:43 +0300

    Global variables in a container

--- solar_main.py
+++ solar_main.py
@@ -10,80 +10,76 @@ import thorpy
 import time
 import numpy as np
 
-timer = None
+class Container:
+    def __init__(self):
+        self.model_time = 0
+        """Физическое время от начала расчёта.
+        Тип: float"""
+        self.displayed_time = 0
+        self.perform_execution = False
+        """Флаг цикличности выполнения расчёта"""
+        self.alive = True
+        self.browser = 0
+        self.time_scale = 1000.0
+        """Шаг по времени при моделировании.
+        Тип: float"""
+        self.physical_time = 0
+        self.time_step = 0
+        self.time_speed = 0
+        self.space = 0
+        self.start_button = 0
+        self.space_objects = []
+        """Список космических объектов."""
+        self.timer = None
 
-alive = True
 
-perform_execution = False
-"""Флаг цикличности выполнения расчёта"""
-
-model_time = 0
-"""Физическое время от начала расчёта.
-Тип: float"""
-
-time_scale = 1000.0
-"""Шаг по времени при моделировании.
-Тип: float"""
-
-space_objects = []
-"""Список космических объектов."""
-
+c = Container()
 
 def execution(delta):
-    """Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
+    """
+    Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
     а также обновляя их положение на экране.
     Цикличность выполнения зависит от значения глобальной переменной perform_execution.
     При perform_execution == True функция запрашивает вызов самой себя по таймеру через от 1 мс до 100 мс.
     """
-    global model_time
-    global displayed_time
-    recalculate_space_objects_positions([dr.obj for dr in space_objects], delta)
-    model_time += delta
-
+    recalculate_space_objects_positions([dr.obj for dr in c.space_objects], delta)
+    c.model_time += delta
 
 def start_execution():
     """Обработчик события нажатия на кнопку Start.
     Запускает циклическое исполнение функции execution.
     """
-    global perform_execution
-    perform_execution = True
+    c.perform_execution = True
 
 
 def pause_execution():
-    global perform_execution
-    perform_execution = False
+    c.perform_execution = False
 
 
 def stop_execution():
     """Обработчик события нажатия на кнопку Start.
     Останавливает циклическое исполнение функции execution.
     """
-    global alive
-    alive = False
+    c.alive = False
 
 
 def open_file():
     """Открывает диалоговое окно выбора имени файла и вызывает
     функцию считывания параметров системы небесных тел из данного файла.
-    Считанные объекты сохраняются в глобальный список space_objects
+    Считанные объекты сохраняются в глобальный список c.space_objects
     """
-    global space_objects
-    global browser
-    global model_time
-
-    model_time = 0.0
+    c.model_time = 0.0
     in_filename = "solar_system.txt"
-    space_objects = read_space_objects_data_from_file(in_filename)
-    max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
+    c.space_objects = read_space_objects_data_from_file(in_filename)
+    max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in c.space_objects])
     calculate_scale_factor(max_distance)
 
 
 def handle_events(events, menu):
-    global alive
     for event in events:
         menu.react(event)
         if event.type == pg.QUIT:
-            alive = False
+            c.alive = False
 
 
 def slider_to_real(val):
@@ -91,12 +87,10 @@ def slider_to_real(val):
 
 
 def slider_reaction(event):
-    global time_scale
-    time_scale = slider_to_real(event.el.get_value())
+    c.time_scale = slider_to_real(event.el.get_value())
 
 
 def init_ui(screen):
-    global browser
     slider = thorpy.SliderX(100, (-10, 10), "Simulation speed")
     slider.user_func = slider_reaction
     button_stop = thorpy.make_button("Quit", func=stop_execution)
@@ -134,18 +128,8 @@ def main():
     """Главная функция главного модуля.
     Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
     """
-
-    global physical_time
-    global displayed_time
-    global time_step
-    global time_speed
-    global space
-    global start_button
-    global perform_execution
-    global timer
-
     print('Modelling started!')
-    physical_time = 0
+    c.physical_time = 0
 
     pg.init()
 
@@ -155,18 +139,18 @@ def main():
     last_time = time.perf_counter()
     drawer = Drawer(screen)
     menu, box, timer = init_ui(screen)
-    perform_execution = True
+    c.perform_execution = True
 
-    while alive:
+    while c.alive:
         handle_events(pg.event.get(), menu)
         cur_time = time.perf_counter()
-        if perform_execution:
-            execution((cur_time - last_time) * time_scale)
-            text = "%d seconds passed" % (int(model_time))
+        if c.perform_execution:
+            execution((cur_time - last_time) * c.time_scale)
+            text = "%d seconds passed" % (int(c.model_time))
             timer.set_text(text)
 
         last_time = cur_time
-        drawer.update(space_objects, box)
+        drawer.update(c.space_objects, box)
         time.sleep(1.0 / 60)
 
     print('Modelling finished!')
