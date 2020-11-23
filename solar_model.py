bec1707f6fe521f14941d20f8fd4e9b5500cc459
Tanya Tomashuk <tanyatomashuk@MacBook-Pro-Tanya.local>
2020-11-23 04:33:25 +0300

    PEP8

--- solar_model.py
+++ solar_model.py
@@ -20,7 +20,7 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+        r = max(r, body.R)
         # гравитация
         f = gravitational_constant * body.m * obj.m / r ** 2
         # оси
@@ -60,7 +60,6 @@ def recalculate_space_objects_positions(space_objects, dt):
         calculate_force(body, space_objects)
     for body in space_objects:
         move_space_object(body, dt)
-
 
 
 if __name__ == "__main__":
